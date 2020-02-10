702523a501b044787d474d2e2c25819e7374efc1
sujinleeme <sujinlee.me@gmail.com>
2020-02-10 14:08:19 +0100

    create entry index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import "core-js";
+import "regenerator-runtime/runtime";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <Banner name="Sujin" />
+  </div>,
+  document.getElementById("app")
+);
