37a60c0428c4ebb7ccbbb13ff94d3b356db930b7
sujinleeme <sujinlee.me@gmail.com>
2020-02-10 16:30:22 +0100

    add normalized.css

--- src/container/index.tsx
+++ src/container/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Banner from "../components/Banner";
+import "./Container.scss";
 
-const Container = () => {
-  return <div>This main container.</div>;
+const Container = (): JSX.Element => {
+  return (
+    <div>
+      <Banner name="sujin " />
+      This main container.<button>ddd</button>
+    </div>
+  );
 };
 
 export default Container;
